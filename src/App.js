42272c8506677ffd2d732e4634c5e9eeea1f926e
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-26 23:15:11 +0530

    home page added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    
+  </div>;
 }
 
 export default App;
