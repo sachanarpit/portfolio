c492434b1dba4dc42660eb3cb4c51cff00fca85d
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-26 23:16:54 +0530

    home page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Home } from "./components/Home";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
